be32f6d386b4f9d92e59db588ee5f51ae358355c
Josiel Souza <josielsouza.dj@gmail.com>
2022-02-25 23:25:33 -0300

    feat: add edit and delete function

--- src/components/Tabela.tsx
+++ src/components/Tabela.tsx
@@ -1,34 +1,65 @@
 import Cliente from "../core/Cliente";
+import { IconeEdicao, IconeLixo } from "./Icones";
 
 interface TabelaProps {
   clientes: Cliente[];
+  clienteSelecionado?: (cliente: Cliente) => void;
+  clienteExcluido?: (cliente: Cliente) => void;
 }
 
 export default function Tabela(props: TabelaProps) {
   function renderizarCabecalho() {
     return (
       <tr>
-        <td>Código</td>
-        <td>Nome</td>
-        <td>Idade</td>
+        <td className="text-left p-4">Código</td>
+        <td className="text-left p-4">Nome</td>
+        <td className="text-left p-4">Idade</td>
+        <td className=" p-4">Ações</td>
       </tr>
     );
   }
 
   function renderizarDados() {
     return props.clientes?.map((cliente, i) => {
-     return(
-        <tr key={cliente.id}>
-            <td>{cliente.id}</td>
-            <td>{cliente.nome}</td>
-            <td>{cliente.idade}</td>
+      return (
+        <tr
+          key={cliente.id}
+          className={`${i % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}
+        >
+          <td className="text-left p-4">{cliente.id}</td>
+          <td className="text-left p-4">{cliente.nome}</td>
+          <td className="text-left p-4">{cliente.idade}</td>
+          {renderizarAcoes(cliente)}
         </tr>
-     )
+      );
     });
   }
+  function renderizarAcoes(cliente: Cliente) {
+    return (
+      <td>
+        <button
+          className={`flex justify-center  items-center text-green-600 rounded-full hover: bg-purple-50 p-2 m-1`}
+        >
+          {IconeEdicao}
+        </button>
+        <button
+          className={`flex justify-center  items-center text-red-500 rounded-full hover: bg-purple-50 p-2 m-1`}
+        >
+          {IconeLixo}
+        </button>
+      </td>
+    );
+  }
+
   return (
-    <table>
-      <thead> {renderizarCabecalho()}</thead>
+    <table className="w-full rounded-xl overflow-hidden ">
+      <thead
+        className={`
+      text-gray-100
+      bg-gradient-to-r from-purple-500 to-purple-800`}
+      >
+        {renderizarCabecalho()}
+      </thead>
       <tbody>{renderizarDados()}</tbody>
     </table>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,13 @@ export default function Home() {
     new Cliente("Ana", 34, "1"),
     new Cliente("Bia", 45, "2"),
     new Cliente("Carlos", 26, "3"),
-    new Cliente("Pedro", 19, "4"),
+    new Cliente("Daniel", 20, "4"),
   ];
   return (
     <div
       className={`
       flex justify-center items-center h-screen
-      bg-gradient-to-r from-blue-500 to-purple-500 text-white
+      bg-gradient-to-r from-violet-200 to-purple-500 text-white
     `}
     >
       <Layout titulo="Cadastro Simples">
