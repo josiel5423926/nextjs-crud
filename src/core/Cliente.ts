de64f77c20d8ec2b6dc60b9d91c01174807e85ec
Josiel Souza <josielsouza.dj@gmail.com>
2022-04-03 20:51:32 -0300

    Repository

--- src/core/Cliente.ts
+++ src/core/Cliente.ts
@@ -1,25 +1,27 @@
 export default class Cliente {
-  #id: string;
-  #nome: string;
-  #idade: number;
+  #id: string
+  #nome: string
+  #idade: number
 
   constructor(nome: string, idade: number, id: string = null) {
-    this.#nome = nome;
-    this.#idade = idade;
-    this.#id = id;
+      this.#nome = nome
+      this.#idade = idade
+      this.#id = id
   }
+
   static vazio() {
-    return new Cliente("", 0);
+      return new Cliente('', 0)
   }
+
   get id() {
-    return this.#id;
+      return this.#id
   }
 
   get nome() {
-    return this.#nome;
+      return this.#nome
   }
 
   get idade() {
-    return this.#idade;
+      return this.#idade
   }
 }
